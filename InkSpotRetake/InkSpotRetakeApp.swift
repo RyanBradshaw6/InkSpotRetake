71983844e20a70178923ca5944836e7d57319385
Ryan Bradshaw <bradshr@bc.edu>
2026-05-04 19:30:26 -0400

    Initial Commit

---
+++ InkSpotRetake/InkSpotRetakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSpotRetakeApp.swift
+//  InkSpotRetake
+//
+//  Created by Ryan Bradshaw on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct InkSpotRetakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
